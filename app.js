1c6a7cd48e04a53498ed34e1110562a86aa2dde3
yayner <yayner2002@gmail.com>
2022-10-26 14:41:31 +0300

    Find blogs from DB and pass them into the view

--- app.js
+++ app.js
@@ -21,30 +21,6 @@ mongoose.connect(dbURI).then((result) => {
 //register view engine and a views folder as default
 app.set('view engine', 'ejs')
 
-//to specify different folder like myviews instead of views
-// app.set('views', 'myviews')
-
-//listen for requests
-// app.listen(3000)
-
-
-
-//top middleware runs for all requests
-// app.use((req, res, next) => {
-//   console.log('new request made')
-//   console.log('host: ', req.hostname)
-//   console.log('path: ', req.path)
-//   console.log('method: ', req.method)
-//   next()
-  
-// })
-
-//second middleware
-
-// app.use((req, res, next) => {
-//   console.log('in the second middleware')
-//   next()
-// })
 
 // third party middlware logger
 app.use(morgan('dev'))
@@ -53,57 +29,30 @@ app.use(morgan('dev'))
 
 app.use(express.static('public'))
 
-app.get('/add-blog', (req, res) => {
-  const blog = new Blog({
-    title: "Sources: Man United close to agreeing deal with ex-Liverpool chief Michael Edwards after 'huge offer'",
-    snippet: "Man United are confident of beating Chelsea to the appointment of former Liverpool transfer guru Michael Edwards, sources have told Football Insider.",
-    body: "United are close to agreeing a deal with the 43-year-old and are willing to wait until next summer for him to join.The Premier League giants have made up their minds that Edwards is the right man to oversee their transfer business after so many expensive mistakes in the past and have submitted what is believe to be a “hugely attractive offer” to him."
-  })
 
-  blog.save().then((result) => {
-    res.send(result)
-  }).catch((err) => {
-    console.log(err)
-  })
-})
-
-app.get('/all-blogs', (req, res) => {
-  Blog.find().then((result) => {
-    res.send(result)
-  }).catch((err) => {
-    console.log(err)
-  })
-
-})
-
-app.get('/single-blog', (req, res) => {
-  
-  Blog.findById('6357d8feeec21838ddd6ef24').then((result) => {
-    res.send(result)
-  }).catch(err => console.log(err))
-})
 
 
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yayner teaches React', snippet: 'lorem ispsum dolor sit amet consectelur'},
-    {title: 'Mario balatoli signs for mancity', snippet: 'lorem ispsum dolor sit amet consectelur'},
-    {title: 'lorem ispsum dolor sit amet consectelur', snippet: 'lorem ispsum dolor sit amet consectelur'},
-  ]
-  res.render('index', {title: 'Home', blogs})
+  res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
   res.render('about', {title: "About"})
 })
 
+//blog routes
+
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({createdAt: -1}).then((result) => {
+    res.render('index', {title: 'All Blogs', blogs: result})
+
+  }).catch(err => {
+    console.log(err)
+  })
+})
 app.get('/blogs/create', (req, res) => {
   res.render('create', {title: "Create blog"})
 })
-
-// app.get('/about-us', (req, res) => {
-//   res.redirect('/about')
-// })
 
 //404 page
 app.use((req, res) => {
