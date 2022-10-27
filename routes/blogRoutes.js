d9bdc10588b02df090246edd23862f07a8a60482
yayner <yayner2002@gmail.com>
2022-10-27 16:07:32 +0300

    Implement MVC method

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,55 +1,21 @@
 const express = require('express')
-const Blog = require('../models/blog')
+const {blog_index, blog_create_form, blog_create_post, blog_delete, blog_details } = require('../controllers/blogControllers')
 
 const route = express.Router()
 //blog routes
-route.get('/', (req, res) => {
-  Blog.find().sort({createdAt: -1}).then((result) => {
-    res.render('index', {title: 'All Blogs', blogs: result})
-
-  }).catch(err => {
-    console.log(err)
-  })
-})
+route.get('/', blog_index)
 
 // post blog
-route.post('/', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog.save().then((result) => {
-    res.redirect('/blogs')
-  }).catch((err) => {
-    console.log(err)
-  })
-
-})
+route.post('/', blog_create_post)
 
 // create a new blog handler
-route.get('/create', (req, res) => {
-  res.render('create', {title: "Create a blog"})
-})
+route.get('/create', blog_create_form)
 
 
 //route params
-route.get('/:id', (req, res) => {
-  const id = req.params.id
-  Blog.findById(id).then((result) => {
-    res.render('details', {blog: result, title: "Blog Detail"})
-  }).catch((err) => {
-    console.log(err)
-  })
-
-})
+route.get('/:id', blog_details)
 //delete request handler
-route.delete('/:id', (req, res) => {
-  const id = req.params.id
-
-  Blog.findByIdAndDelete(id).then((result) => {
-    res.json({ redirect: '/blogs'})
-  }).catch((err) => {
-    console.log(err)
-  })
-})
+route.delete('/:id', blog_delete)
 
 
 module.exports = route
